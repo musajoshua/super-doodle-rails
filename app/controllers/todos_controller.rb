089d9c37ebc6aecf839944b4b19aca8474df0608
Musa Joshua <gidijosh@gmail.com>
2024-02-26 19:19:07 +0100

    basic crud todo app in rails

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,43 @@
 class TodosController < ApplicationController
+  before_action :find_and_set_todo, only:[:show, :edit, :update, :destroy]
+  def index
+    @todos = Todo.all
+  end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_param)
+    if @todo.save
+      redirect_to todos_path
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @todo.update(todo_param)
+      redirect_to todos_path, notice: 'Todo was successfully updated.'
+    end
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  private
+  def find_and_set_todo
+    @todo = Todo.find(params[:id])
+  end
+
+  def todo_param
+    params.require(:todo).permit(:title, :status)
+  end
 end
--- db/migrate/20240223153219_create_todos.rb
+++ db/migrate/20240223153219_create_todos.rb
@@ -1,7 +1,7 @@
 class CreateTodos < ActiveRecord::Migration[7.1]
   def change
     create_table :todos do |t|
-      t.text_field :title
+      t.string :title
       t.boolean :status
 
       t.timestamps
